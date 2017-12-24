6a92acf576450ca5e5997f49f4aa67c7e442294c
Rei Shimizu <Shikugawa@gmail.com>
2017-12-25 00:52:07 +0900

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SpectrogramWithFft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resultMessageBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 268);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(675, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(675, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.fileToolStripMenuItem.Text = "Start";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // resultMessageBox
+            // 
+            this.resultMessageBox.Location = new System.Drawing.Point(12, 27);
+            this.resultMessageBox.Name = "resultMessageBox";
+            this.resultMessageBox.Size = new System.Drawing.Size(651, 223);
+            this.resultMessageBox.TabIndex = 2;
+            this.resultMessageBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 290);
+            this.Controls.Add(this.resultMessageBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SpectrogramWithFft";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox resultMessageBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using Sound.WAVE;
+using Signal.FrequencyAnalysis;
+using Graphic.ColorClass;
+
+namespace SpectrogramWithFft
+{
+    public partial class Form1 : Form
+    {
+        WaveReader reader = new WaveReader();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(
+                () =>
+                {
+                    string basePath = @"C:\Users\shikugawa\Downloads\test\audio";
+                    IEnumerable<string> files = Directory.EnumerateFiles(basePath, "*", SearchOption.AllDirectories);
+
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(file);
+                        var regexp = new Regex(@"audio\\(.+)\\(.+)");
+                        var matchedData = regexp.Match(file);
+
+                        var folderName = matchedData.Groups[1];
+                        var fileName = matchedData.Groups[2];
+
+                        if (!Directory.Exists("C:\\Users\\shikugawa\\Documents\\Test\\" + folderName))
+                        {
+                            Directory.CreateDirectory("C:\\Users\\shikugawa\\Documents\\Test\\" + folderName);
+                        }
+
+                        this.reader.Open(file);
+
+
+                        if (this.reader.IsOpen && this.reader.ReadLimit == false)
+                        {
+                            int i = 0;
+                            List<FFTresult> listBuff = new List<FFTresult>();
+                            while (i < 16)                                   
+                            {
+                                var data = this.reader.Read(1024);  
+                                double[] sound;
+                                if (data.UsableCH == MusicData.UsableChannel.Left)
+                                    sound = data.GetData(MusicData.Channel.Left);
+                                else
+                                    sound = data.GetData(MusicData.Channel.Right);
+
+                                var dft = new DFT(sound.Length, Window.WindowKind.Hanning);
+                                FFTresult result = dft.FFT(sound, (long)this.reader.SamplingRate);
+                                listBuff.Add(result);
+
+                                i++;
+                            }
+                            
+                            double max = double.MinValue;
+                            for (int j = 0; j < listBuff.Count; j++)
+                                if (max < listBuff[j].MaxAbs) max = listBuff[j].MedianAbs;
+                            int height = 512;
+
+                            Bitmap img = new Bitmap(listBuff.Count, height);
+
+                            foreach(var l in listBuff)
+                            {
+                                Debug.WriteLine(l);
+                            }
+
+                            Colormap cmap = new Colormap();
+                            for (int k = 0; k < height; k++)
+                            {
+                                for (int j = 0; j < listBuff.Count; j++)
+                                {
+                                    int index = listBuff[j].Length * k / height;
+                                    double power = 3.0;             
+                                    var value = (Math.Log10(listBuff[j].GetData(index).data.Abs / max) + power) / (power * 2.0);
+                                    if (value > 1.0) value = 1.0;
+                                    if (value < 0.0) value = 0.0;
+                                    Color c = cmap.GetColor(value);
+                                    
+                                        img.SetPixel(j, height - k - 1, c);
+                                    
+                                }
+                            }
+
+                            Action appendText = () => resultMessageBox.AppendText("succeeded!: FolderName->" + folderName + " FileName->" + fileName + "\n");
+                            Invoke(appendText);
+                            img.Save("C:\\Users\\shikugawa\\Documents\\Test\\" + folderName + "\\" + fileName + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                        }
+                    }
+                }
+            );
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
